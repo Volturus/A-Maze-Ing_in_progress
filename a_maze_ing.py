7475cdbe3fc73b83f2ad682fdad72c7b79ac2eeb
Windjigo <42441775+Volturus@users.noreply.github.com>
2026-04-27 15:07:56 +0200

    Main + basic user interaction to complete

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -2,6 +2,25 @@ import hunt_kill_maze_algo
 import interpreter
 import output_file
 import parser
+import sys
+
+
+def make_choice():
+    print("\n=== A-Maze-Ing ===")
+    print("\n1. Re-generate a new maze using config.txt\n2. Show/Hide path from entry to exit")
+    print("3. Rotate maze colors\n4. Quit\nChoice? (1-4)\n")
+    choice = sys.stdin.readline().strip()
+    if (choice == "1"):
+        main()
+    elif (choice == "2"):
+        print("\nWork in progress, please choose something else")
+        make_choice()
+    elif (choice == "3"):
+        print("\nWork in progress, please choose something else")
+        make_choice()
+    elif (choice != "4"):
+        print("\nInvalid input. Please enter a number between 1 and 4")
+    return
 
 
 def main():
@@ -10,6 +29,7 @@ def main():
     maze.hunt_kill_algo()
     interpreter.interpreter(maze.maze, maze_config.entry[1]*maze_config.width + maze_config.entry[0] + 1, maze_config.exit_[1]*maze_config.width + maze_config.exit_[0] + 1)
     output_file.output_file(maze, maze_config.entry, maze_config.exit_)
+    make_choice()
 
 
 if __name__ == "__main__":
